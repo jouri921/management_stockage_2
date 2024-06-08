b89e2f5e3386dda93e0c07aa2ae8737e5a04876c
jouri921 <158305118+jouri921@users.noreply.github.com>
2024-06-08 10:47:55 +0100

    Update settings.py
    j

--- mangagement_stockage_eccomerce/settings.py
+++ mangagement_stockage_eccomerce/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',  # Ensure this middleware is included
     'dashboard.middleware.LoginCheckMiddleware',
 ]
 
